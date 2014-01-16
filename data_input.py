77266bb49b233cd11846007bc9180b0a8c50dbe3
DSBVengeance <dsb.vengeance@gmail.com>
2014-01-16 10:14:41 +0000

    Data Input Update
    Hopefully finished (probably not)

--- data_input.py
+++ data_input.py
@@ -24,18 +24,23 @@ def dress_type_input():
     Length = input("Please enter the length of the Dress (cm): ")
     return Detail, Fabric, Length
 
+def item_type_input():
+    ItemType = input("Please enter the type of item: ")
+    return ItemType
+
 def appointment_input(db_name):
     Date = input("Please enter the time of appointment: ")
     Time = input("Please enter the date of appointment: ")
-    Customer = select_data(1, db_name)
-    Type = select_data(3, db_name)
+    Customer = select_data(4, db_name)
+    Type = select_data(9, db_name)
     return Customer, Type, Date, Time
 
 def item_input(db_name):
-    BodiceType = select_data(4,db_name)
-    Customer = select_data(1,db_name)
+    BodiceType = select_data(3,db_name)
+    Customer = select_data(4,db_name)
     DressType = select_data(5,db_name)
-    Status = select_data(2,db_name)
+    Status = select_data(8,db_name)
+    ItemType = select_data(7, db_name)
     Bridal = input("Is this a bridal item?: ")
     DateIn = input("Please enter the Date the item came in: ")
     DateRequired = input("Please enter the date the item in required (if applicable): ")
@@ -43,6 +48,6 @@ def item_input(db_name):
     OtherRequirements = input("Please enter any otger requirements: ")
 
 def appointment_item(db_name):
-    Appointment = select_data(6,db_name)
-    Item = select_data(7,db_name)
+    Appointment = select_data(1,db_name)
+    Item = select_data(6,db_name)
     return Appointment,Item
