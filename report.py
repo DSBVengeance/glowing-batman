e7da31af01997f457dadeea91e2fc203a0bfccc2
DSBVengeance <DSB.Vengeance@gmail.com>
2014-01-21 10:39:59 +0000

    More changes

--- report.py
+++ report.py
@@ -1,6 +1,11 @@
 import sqlite3
 
+
+
+
 if choice == 1:
-    print('{0} {1} {2} {3} {4}'.format('Customer Name', 'Date','Time','Customer Telephone No.'))
+    
+    print('{0:>7} {1:>5} {2:>5} {3:>12}'.format('Customer Name', 'Date','Time','Customer Telephone No.'))
     for n in range(len(temp)):
-        print('{0} {1} {2} {3} {4}')
+        print('{0} {1} {2} {3}'.format(CustomerName,temp[n]))
+
